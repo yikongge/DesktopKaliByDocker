32450c962d1e7a26fde1eab2e187a0b804ec4856
yikongge <yangjianchen333@163.com>
2020-02-12 11:54:16 +0800

    update

--- kali.sh
+++ kali.sh
@@ -2,13 +2,25 @@
 #echo $#
 ze=$#
 gu="GUI"
+in="init"
 #if (( $ze == 0 ))
+xhost +
 if [ "$ze" -eq "0" ]
 then
  docker exec -it kali /bin/bash
 elif [ "$1"x = "$gu"x ]
 then
+ #docker exec -d kali rm /tmp/.X2-lock
+ docker exec -d kali vncserver -kill :2
+ echo 'closed'
+ sleep 1
+ docker exec -d kali vncserver -geometry 1366x768 :2
+ sleep 1
  remmina -c kali.remmina
+elif [ "$1"x = "$in"x ]
+then
+ docker exec -t kali rm /tmp/.X2-lock
+ docker exec -t kali vncserver -geometry 1366x768 :2
 else
  command='docker exec -t kali '$@
  $command
